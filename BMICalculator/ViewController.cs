a648d197f2a783e06853abeaa59c00203d70203c
Alberto <alberto@mendezcabrera.com>
2016-03-24 21:16:05 +0100

    Initial commit. iOS app created

---
+++ BMICalculator/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using UIKit;
+
+namespace BMICalculator
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			calculateButton.TouchUpInside += CalculateButton_TouchUpInside;
+		}
+
+		void CalculateButton_TouchUpInside (object sender, EventArgs e)
+		{
+			float height = float.Parse(heightTextField.Text);
+			float weight = float.Parse (weightTextField.Text);
+
+			float bmi = (weight / (height * height));
+
+			bmiLabel.Text = bmi.ToString ();
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		public override void TouchesBegan (Foundation.NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan (touches, evt);
+
+			this.View.EndEditing (true);
+		}
+	}
+}
+
